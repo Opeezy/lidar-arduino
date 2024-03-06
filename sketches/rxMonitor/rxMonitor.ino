b30aaba893a34e77c1cd93334df92a99843f52eb
Chris Openshaw <copenshaw17@gmail.com>
2024-03-06 15:55:29 -0600

    madness setting in

--- sketches/rxMonitor/rxMonitor.ino
+++ sketches/rxMonitor/rxMonitor.ino
@@ -1,9 +1,22 @@
+
+byte msp = 0;
+byte lsp = 0;
+byte shift = 1;
+byte packet[2];
+int count = 0;
+
 void setup() {
   //Setup serial
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.write(digitalRead(0));
-  delay(100);
+  if (count != 11) {
+      Serial.write(digitalRead(0));
+      count++;
+  } else {
+    Serial.print("\n");
+    count = 0;
+  }
+  delay(10);
 }
