c9e043824bd110b33fa71d31c45076e991aba810
Chris Openshaw <copenshaw17@gmail.com>
2024-03-11 08:30:34 -0500

    ,

--- src/device/device.ino
+++ src/device/device.ino
@@ -3,19 +3,20 @@ const byte appOn = 3;
 const byte writeReady = 4;
 const byte readReady = 5;
 
+bool connStatus = false;
 void setup() {
   //Setup serial
   Serial.begin(230400);
-  mySerial.begin(230400);
   delay(1000);
 }
 
 void loop() {
-  clearSerialBuffer();
-
-  Serial.flush();-
-  delay(50);
+  if (!connStatus) {
+    establishConnection();
+  } else {
+    //TODO
+  }
 }
 bool establishConnection() {
-  
+  //TODO: Send byte to app to establish connection
 }
