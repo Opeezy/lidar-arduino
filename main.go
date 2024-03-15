80cc4366b0a7303637486c779b77b770108044f2
Chris Openshaw <copenshaw17@gmail.com>
2024-03-15 11:02:30 -0500

    modules

--- main.go
+++ main.go
@@ -1,98 +1,94 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"time"
-
-	"github.com/tarm/serial"
 )
 
-type Serial struct {
-	name       string         // Serial port name (eg. "COM5")
-	config     *serial.Config // Serial config pointer
-	port       *serial.Port   // Serial port pointer
-	configMade bool           // Valid config status
-	portOpen   bool           // Port connection status
-}
+// type Serial struct {
+// 	name       string         // Serial port name (eg. "COM5")
+// 	config     *serial.Config // Serial config pointer
+// 	port       *serial.Port   // Serial port pointer
+// 	configMade bool           // Valid config status
+// 	portOpen   bool           // Port connection status
+// }
 
-type SerialWorker interface {
-	openPortConnection()
-	makeConfig()
-	listen()
-	read()
-}
+// type SerialWorker interface {
+// 	openPortConnection()
+// 	makeConfig()
+// 	listen()
+// 	read()
+// }
 
-func (s *Serial) openPortConnection() error {
-	if s.configMade {
-		port, err := serial.OpenPort(s.config)
-		s.port = port
-		if err != nil {
-			return err
-		} else {
-			log.Printf("Successfully opened port %v", s.name)
-			s.portOpen = true
-			return nil
-		}
-	} else {
-		err := errors.New("No config made")
-		return err
-	}
-}
+// func (s *Serial) openPortConnection() error {
+// 	if s.configMade {
+// 		port, err := serial.OpenPort(s.config)
+// 		s.port = port
+// 		if err != nil {
+// 			return err
+// 		} else {
+// 			log.Printf("Successfully opened port %v", s.name)
+// 			s.portOpen = true
+// 			return nil
+// 		}
+// 	} else {
+// 		err := errors.New("No config made")
+// 		return err
+// 	}
+// }
 
-func (s *Serial) makeConfig(name string, baud int, size byte, stopbits serial.StopBits, timeout time.Duration) {
-	newConfig := &serial.Config{
-		Name:        name,            // Port name (eg. "COM5")
-		Baud:        baud,            // Baudrate
-		Size:        size,            // Data size (usually 8 bytes)
-		StopBits:    stopbits,        // Stop bits (usually 1)
-		ReadTimeout: time.Second * 5} // Timeout duration
-	s.config = newConfig
-	s.name = newConfig.Name
-	s.configMade = true
-	log.Println("Config generated")
-}
+// func (s *Serial) makeConfig(name string, baud int, size byte, stopbits serial.StopBits, timeout time.Duration) {
+// 	newConfig := &serial.Config{
+// 		Name:        name,            // Port name (eg. "COM5")
+// 		Baud:        baud,            // Baudrate
+// 		Size:        size,            // Data size (usually 8 bytes)
+// 		StopBits:    stopbits,        // Stop bits (usually 1)
+// 		ReadTimeout: time.Second * 5} // Timeout duration
+// 	s.config = newConfig
+// 	s.name = newConfig.Name
+// 	s.configMade = true
+// 	log.Println("Config generated")
+// }
 
-func (s *Serial) listen(duration time.Duration) {
-	if !s.configMade {
-		log.Fatalln("No config made")
-	} else if !s.portOpen {
-		log.Fatalln("No port connection")
-	} else {
-		log.Println("Reading serial data")
-		timeNow := time.Now() // Get the current time
+// func (s *Serial) listen(duration time.Duration) {
+// 	if !s.configMade {
+// 		log.Fatalln("No config made")
+// 	} else if !s.portOpen {
+// 		log.Fatalln("No port connection")
+// 	} else {
+// 		log.Println("Reading serial data")
+// 		timeNow := time.Now() // Get the current time
 
-		data := make(chan []byte) // Channel to pass our packets through
+// 		data := make(chan []byte) // Channel to pass our packets through
 
-		for {
-			// Check for duration and read serial data until it is reached
-			if time.Since(timeNow) >= duration {
-				log.Fatalln("5 seconds passed")
-				break
-			}
-			go s.readSerial(data)
-			go s.writeToFile(data)
-		}
-	}
-}
+// 		for {
+// 			// Check for duration and read serial data until it is reached
+// 			if time.Since(timeNow) >= duration {
+// 				log.Fatalln("5 seconds passed")
+// 				break
+// 			}
+// 			go s.readSerial(data)
+// 			go s.writeToFile(data)
+// 		}
+// 	}
+// }
 
-func (s *Serial) readSerial(c chan []byte) {
-	buf := make([]byte, 47) // 47 byte buffer (the size of our lidar packets come out to 47 bytes)
+// func (s *Serial) readSerial(c chan []byte) {
+// 	buf := make([]byte, 47) // 47 byte buffer (the size of our lidar packets come out to 47 bytes)
 
-	i, _ := s.port.Read(buf) // Read incoming serial data into buffer
-	_ = i
+// 	i, _ := s.port.Read(buf) // Read incoming serial data into buffer
+// 	_ = i
 
-	// Check first two bytes as they are always static. 0x54 and 0x2C indicate a complete packet
-	if buf[0] == 0x54 && buf[1] == 0x2C {
-		c <- buf // Send our packet to channel
-	}
-}
+// 	// Check first two bytes as they are always static. 0x54 and 0x2C indicate a complete packet
+// 	if buf[0] == 0x54 && buf[1] == 0x2C {
+// 		c <- buf // Send our packet to channel
+// 	}
+// }
 
-func (s *Serial) writeToFile(c chan []byte) {
-	packet := <-c
-	fmt.Println(packet)
-}
+// func (s *Serial) writeToFile(c chan []byte) {
+// 	packet := <-c
+// 	fmt.Println(packet)
+// }
 
 func main() {
 	// Initialize our Serial component
