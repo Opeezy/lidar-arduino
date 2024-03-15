9537fa3b2def3ac71d2db833c7f3b09b48b1ed69
Chris Openshaw <copenshaw17@gmail.com>
2024-03-15 14:25:55 -0500

    ok modules are back

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"lidar-arduino/serial"
 	"log"
 	"time"
 )
 
 func main() {
 	// Initialize our Serial component
-	Serial1 := NewSerial("COM5", 230400, 8, 1, time.Second*5)
+	Serial1 := serial.NewSerial("COM5", 230400, 8, 1, time.Second*5)
 
 	// Check for valid config and attempt to open serial port connection
 	err := Serial1.OpenPortConnection()
